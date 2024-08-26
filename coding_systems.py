bf668d0f2c45ce20d7b9be05e605288b1cd6be0b
pravboi <pravachanpatra@gmail.com>
2024-08-26 16:16:29 -0500

    gray code to binary

--- coding_systems.py
+++ coding_systems.py
@@ -9,8 +9,8 @@ represented as string ""1010101_GC"
 """
 from conversions_number_systems import decimal_to_binary, strip_suffix, BASE_10, BASE_2
 
-BCD = "BCD"
-GC = "GC"
+BCD = "_BCD"
+GC = "_GC"
 
 # if binary string is not 4 digits adds zeros to prefix, pass in withou suffix
 def get_4_bits(binary):
@@ -38,11 +38,10 @@ def decimal_to_BCD(decimal):
         digit_binary_4_bits = get_4_bits(digit_binary_code)
         bcd_str += digit_binary_4_bits
     
-    return bcd_str + "_"+BCD
+    return bcd_str +BCD
 
 def binary_to_gray_code(binary):
     binary = strip_suffix(binary) 
-    print(binary)
     gray_code = binary[0]  # drop down first bit
 
     for i in range(len(binary)):
@@ -51,12 +50,24 @@ def binary_to_gray_code(binary):
             gray_code_bit = XOR(bit1, bit2)
             gray_code += gray_code_bit
     
-    return gray_code + "_"+GC
+    return gray_code + GC
+
+def gray_code_to_binary(gray_code):
+    gray_code = strip_suffix(gray_code)
+    binary = gray_code[0]
+
+    for i in range(len(gray_code)):
+        if i-1 >= 0:
+            bit1, bit2 = binary[i-1], gray_code[i]
+            binary_bit = XOR(bit1, bit2)
+            binary += binary_bit
+
+    return binary + BASE_2
 
 
 def main():
     print("\nConvert decimal to BCD")
-    decimal = "123_" + BCD
+    decimal = "123" + BCD
     binary = decimal_to_BCD(decimal)
     print(f"decimal {decimal} is BCD {binary}")
 
@@ -64,6 +75,11 @@ def main():
     binary = "11101" + BASE_2
     gray_code = binary_to_gray_code(binary)
     print(f"binary {binary} is GC {gray_code}")
+
+    print("\nConvert Gray Code to binary")
+    gray_code = "10011" + GC
+    binary = gray_code_to_binary(gray_code)
+    print(f"binary {gray_code} is GC {binary}")
 
 
 
